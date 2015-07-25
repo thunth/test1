deb7a94ca23fea447991d08e5221989dd39c38ca
Hoai Thu <nguyenthihoaithu1923@gmail.com>
2015-07-25 15:39:19 +0700

    test1

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: hoaithu
+ *
+ * Created on July 25, 2015, 3:31 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
